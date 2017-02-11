e40be527bf0386b93161f06c302b46efe98a5131
Anders Thøgersen <anders@bladre.dk>
2017-02-11 12:45:47 +0100

    test

--- example/test.js
+++ example/test.js
@@ -1,4 +1,12 @@
 function test() {
-    console.warn('Fresh FOO');
+  console.warn('Fresh FOO', {
+    a: [23, {
+      a:[1,2,3],
+      f: {
+        aaaa: 12121
+      }
+    }
+    ]
+  });
 }
 setTimeout(test, 1000);
